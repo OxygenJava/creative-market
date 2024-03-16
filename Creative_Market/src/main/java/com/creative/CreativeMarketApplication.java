f656c2f42d95c05253755927b6bf945426bbe116
githubzhengjinwei <1582040682@qq.com>
2024-03-16 18:00:11 +0800

    输出555

--- Creative_Market/src/main/java/com/creative/CreativeMarketApplication.java
+++ Creative_Market/src/main/java/com/creative/CreativeMarketApplication.java
@@ -8,7 +8,7 @@ public class CreativeMarketApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("666");
+        System.out.println("555");
 
         SpringApplication.run(CreativeMarketApplication.class, args);
     }
