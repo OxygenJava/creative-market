29ab3680954cc34e4832875f5c9b92adf0ac1557
githubzhengjinwei <1582040682@qq.com>
2024-03-24 22:52:44 +0800

    修改了错误

--- Creative_Market/src/main/java/com/creative/config/webmvcConfig.java
+++ Creative_Market/src/main/java/com/creative/config/webmvcConfig.java
@@ -27,7 +27,7 @@ public class webmvcConfig implements WebMvcConfigurer {
                 "/api/commodity/**",
                 "api/lable/**",
                 "/api/post/**",
-                "/api/likepost/**"
+                "/api/likepost/**",
                 "/api/crow/**",
                 "/api/crow/team/**",
                 "/api/homePage/**",
