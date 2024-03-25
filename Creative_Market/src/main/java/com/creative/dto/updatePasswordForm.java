18bc574e552d670e821a2ecbbab40f90d1cc00e7
oxygenff <2423063401@qq.com>
2024-03-25 18:04:13 +0800

    修复修改密码的bug

--- Creative_Market/src/main/java/com/creative/dto/updatePasswordForm.java
+++ Creative_Market/src/main/java/com/creative/dto/updatePasswordForm.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 public class updatePasswordForm {
-    private Integer id;
     private String originalPassword;
     private String newPassword;
     private String confirmNewPassword;
