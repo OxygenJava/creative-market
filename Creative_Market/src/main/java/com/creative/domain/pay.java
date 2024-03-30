d67ca5afd3de164c0bcc8a6741966499b51987e1
storypore <2807063694@qq.com>
2024-03-31 00:11:45 +0800

    订单系统大致完成

--- Creative_Market/src/main/java/com/creative/domain/pay.java
+++ Creative_Market/src/main/java/com/creative/domain/pay.java
@@ -2,6 +2,7 @@ package com.creative.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,9 @@ public class pay {
     @TableId(type = IdType.AUTO)
     private Integer payID;
     private Integer payMoney;
-    private String payType;
+    private Integer payType;
+    @JsonFormat(pattern ="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime payTime;
+    private Integer userId;
+    private Integer commodityId;
 }
--- Creative_Market/src/main/java/com/creative/mapper/orderMapper.java
+++ Creative_Market/src/main/java/com/creative/mapper/orderMapper.java
@@ -1,8 +1,9 @@
 package com.creative.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.creative.domain.orderTable;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface orderMapper extends BaseMapper {
+public interface orderMapper extends BaseMapper<orderTable> {
 }
