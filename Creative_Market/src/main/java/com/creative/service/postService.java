a9d759716d58688856c586b92765fca3f7fc40f2
githubzhengjinwei <1582040682@qq.com>
2024-04-17 13:23:58 +0800

    修改了帖子模糊查询的业务

--- Creative_Market/src/main/java/com/creative/service/postService.java
+++ Creative_Market/src/main/java/com/creative/service/postService.java
@@ -43,5 +43,5 @@ public interface postService {
      */
     Result getPostDetail(Integer postId);
 
-    Result selectLikePost(Integer pageSize, Integer pageNumber,post post);
+    Result selectLikePost(Integer pageSize, Integer pageNumber,post post,HttpServletRequest request);
 }
